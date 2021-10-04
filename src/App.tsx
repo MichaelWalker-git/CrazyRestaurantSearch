17ce41f478e5c67fcd6e17638f23372938952c3e
miketran <mtran82@gatech.edu>
2021-10-03 22:44:33 -0700

    Join the data, render it

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,42 @@ import React, { ChangeEvent, createRef, Ref, useEffect, useMemo, useState } from
 import logo from './logo.png';
 import './App.scss';
 import { getYelpResults } from './service/yelpService';
-import { getLatLong, getLocationAutoComplete } from './service/googleMapService';
+import {
+  getGooglePhotoRef,
+  getGoogleRestaurantResults,
+  getLatLong,
+  getLocationAutoComplete
+} from './service/googleMapService';
 
 import debounce from 'lodash.debounce';
 import { Button, Card, Col, Container, Dropdown, FormControl, InputGroup, Row } from 'react-bootstrap';
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
-import { GoogleAutoCompletePrediction, YelpBusiness } from './types';
+import {GoogleAutoCompletePrediction, GoogleBusiness, YelpBusiness} from './types';
 import { Type } from 'typescript';
 
 const BusinessResult = (props: any) => {
+  const [googleUrlImage, setUrlImage] = useState("");
   const { name, location, rating, image_url, review_count } = props.yelpBiz;
-
+  const { user_ratings_total, } = props.googleBiz;
+  const googleRating = props.googleBiz.rating;
   //TODO(miketran): Google reviews
   //TODO(miketran): Restaurant website
+
+  // function validateResponse(response: any) {
+  //   if (!response.ok) {
+  //     throw Error(response.statusText);
+  //   }
+  //   return response;
+  // }
+
+  // const googleImage = getGooglePhotoRef(props.googleBiz.photos[0].photo_reference)
+  //   .then(validateResponse)
+  //   .then(response => response?.blob())
+  //   .then(blob => {
+  //     setUrlImage(URL.createObjectURL(blob))
+  //     console.log(URL.createObjectURL(blob))
+  //   })
+
   return (
     <>
       <Card>
@@ -27,12 +50,11 @@ const BusinessResult = (props: any) => {
                 <Card.Img variant="top" src={image_url} />
               </Col>
               <Col>
-                <h5>Google Rating: {rating} of 5 stars ({review_count} total reviews)</h5>
-                <Card.Img variant="top" src={image_url} />
+                <h5>Google Rating: {googleRating} of 5 stars ({user_ratings_total} total reviews)</h5>
+                <Card.Img variant="top" src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=Aap_uEACyFoIIiPqphiR3VgwJhoQlv9S4_AWIiFYcr18UKG-j_FpXunyRaeS7ErEvP6BEO49dMFnLURs0HIOQqgPzF8D3tmDr5aFsOXNMI3l-98I8hOoDhfgD3ylUklBo45U1B6RQSRbjalhtHraoB-1qoU-KpKK7pra6S7ruxi-Tto5WEHq&key=AIzaSyAjf7dv23CHDQtTyldde3N3BJpYxXXFnkE`} />
               </Col>
             </Row>
           </Container>
-
         </Card.Body>
       </Card>
       <Dropdown.Divider />
@@ -43,19 +65,23 @@ const BusinessResult = (props: any) => {
 
 //TODO(miketran): Add Correct Type
 const SearchResultBody = (props: any) => {
-
-  // join yelp and google data
+  console.log(props, "PROPS in SearchResultBody")
   return (
     <div>
-      {props.searchResults?.length > 0 && props.searchResults.map((biz: YelpBusiness) => {
-        return (<BusinessResult yelpBiz={biz} key={biz.id} />)
-      })}
+      {props.searchResults?.size > 0 &&
+      [...props.searchResults.values()].map((bizTupal: [YelpBusiness,GoogleBusiness]) => {
+        return (<BusinessResult
+          yelpBiz={bizTupal[0]}
+          googleBiz={bizTupal[1]}
+          key={bizTupal[0].id} />
+        )})
+      }
     </div>
   );
 }
 
 interface SearchBarProps {
-  handleSearchResults: (a: Array<YelpBusiness>) => void;
+  handleSearchResults: (a: Map<string, Array<YelpBusiness|GoogleBusiness>>) => void;
   latLong: Array<string>;
   searchAreaTerm: Array<string>;
   searchBoxTerm: string;
@@ -101,10 +127,12 @@ function SearchBar(props: SearchBarProps) {
    */
   const handleSearch = (evt: any) => {
     evt.preventDefault();
-    console.log(props.searchAreaTerm, "handleSearch", props)
-
-    getYelpResults(props.searchBoxTerm, props.latLong[0], props.latLong[1]).then((res) => {
-      props.handleSearchResults(res.businesses)
+    Promise.all([
+      getYelpResults(props.searchBoxTerm, props.latLong[0], props.latLong[1]),
+      getGoogleRestaurantResults(`${props.latLong[1]},${props.latLong[0]}`,props.searchBoxTerm )
+    ]).then((res) => {
+      const newDataMap = joinDataSources(res);
+      props.handleSearchResults(newDataMap)
     })
   }
 
@@ -144,21 +172,44 @@ function SearchBar(props: SearchBarProps) {
   );
 }
 
+const joinDataSources = (res: [{businesses: Array<YelpBusiness>}, {results: Array<GoogleBusiness>}]) => {
+  const resultMap = new Map();
+  const joinedMap = new Map();
+  const yelpResults = res[0].businesses;
+  const googleResults = res[1].results;
+  for (let i = 0; i < yelpResults.length; i++) {
+    if(yelpResults[i].location.address1?.length > 0){
+      resultMap.set(yelpResults[i].location.address1, [yelpResults[i]]);
+    }
+  }
+  for (let j = 0; j < googleResults.length; j++) {
+    const firstPartAddress = googleResults[j].vicinity.split(",")[0];
+    if(resultMap.has(firstPartAddress)){
+      const currentBizArr = resultMap.get(firstPartAddress);
+      joinedMap.set(firstPartAddress, [...currentBizArr, googleResults[j]]);
+    }
+  }
+  return joinedMap;
+}
+
 function App() {
   const [searchTerm, setSearchTerm] = useState("Restaurants");
   const [searchArea, setSearchArea] = useState(["San Francisco, CA"])
-  const [searchResults, setSearchResults] = useState<Array<YelpBusiness>>([]);
+  const [searchResults, setSearchResults] = useState<Map<string, Array<YelpBusiness|GoogleBusiness>>>(new Map());
   const [latLongSelectedCity, setLatLong] = useState(["-122.4194", "37.7749"]);
 
   useEffect(() => {
     Promise.all([
       getYelpResults(searchTerm, latLongSelectedCity[0], latLongSelectedCity[1]),
+      getGoogleRestaurantResults(`${latLongSelectedCity[1]},${latLongSelectedCity[0]}`,searchTerm )
     ]).then((res) => {
-      setSearchResults(res[0].businesses);
+      setSearchResults(joinDataSources(res));
     })
   }, [])
 
-  const handleSearchResults = (searchResultFromButton: Array<YelpBusiness>) => {
+
+
+  const handleSearchResults = (searchResultFromButton: Map<string, Array<YelpBusiness|GoogleBusiness>>) => {
     setSearchResults(searchResultFromButton);
   }
 
@@ -172,7 +223,8 @@ function App() {
       </header>
       <main className={"mainBody"}>
         <div>
-          <SearchBar handleSearchResults={handleSearchResults}
+          <SearchBar
+            handleSearchResults={handleSearchResults}
             searchBoxTerm={searchTerm}
             searchAreaTerm={searchArea}
             latLong={latLongSelectedCity}
--- src/types.ts
+++ src/types.ts
@@ -1,45 +1,70 @@
 interface YelpBusinessCooodinates {
-    latitude: number;
-    longitude: number;
+  latitude: number;
+  longitude: number;
 }
 
 interface YelpBusCategory {
-    alias: string;
-    title: string;
+  alias: string;
+  title: string;
 }
 
 interface YelpBusAddress {
-    address1: string;
-    address2: string;
-    address3: string;
-    city: string;
-    country: string;
-    display_address: Array<string>;
-    state: string;
-    zip_code: string;
+  address1: string;
+  address2: string;
+  address3: string;
+  city: string;
+  country: string;
+  display_address: Array<string>;
+  state: string;
+  zip_code: string;
 }
 
 export interface YelpBusiness {
-    alias: string
-    categories: Array<YelpBusCategory>;
-    coordinates: YelpBusinessCooodinates;
-    display_phone: string;
-    distance: number;
-    id: string;
-    image_url: string;
-    is_closed: boolean;
-    location: YelpBusAddress;
-    name: string;
-    phone: string;
-    price: string;
-    rating: number
-    review_count: Number;
-    transactions: Array<string>;
-    url: string;
+  alias: string
+  categories: Array<YelpBusCategory>;
+  coordinates: YelpBusinessCooodinates;
+  display_phone: string;
+  distance: number;
+  id: string;
+  image_url: string;
+  is_closed: boolean;
+  location: YelpBusAddress;
+  name: string;
+  phone: string;
+  price: string;
+  rating: number
+  review_count: Number;
+  transactions: Array<string>;
+  url: string;
 }
 
 export interface GoogleAutoCompletePrediction {
-    description: string;
-    place_id: string;
-    reference: string;
+  description: string;
+  place_id: string;
+  reference: string;
+}
+
+export interface GoogleBusiness {
+  business_status: string;
+  geometry: { location: { lat: number, lng: number } }
+  icon: string;
+  icon_background_color: string;
+  icon_mask_base_uri: string;
+  name: string;
+  opening_hours: { open_now: boolean }
+  photos: [{
+    height: number;
+    html_attributions: Array<string>;
+    photo_reference: string;
+    width: number;
+  }]
+  place_id: string;
+  plus_code: { compound_code: string, global_code: string }
+  price_level: number;
+  rating: number;
+  reference: string;
+  scope: string;
+  types: Array<string>
+  user_ratings_total: number
+  vicinity: string;
 }
