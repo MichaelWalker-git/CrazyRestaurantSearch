e5e8b2cd5f09ac326139ea5ca68bfd0775946fde
miketran <mtran82@gatech.edu>
2021-10-07 13:34:40 -0700

    [[styling] - breakpoints for pictures

--- src/components/BusinessResult.tsx
+++ src/components/BusinessResult.tsx
@@ -15,11 +15,11 @@ export const BusinessResult = (props: any) => {
             <Row>
               <Card.Title>{name} : {location.address1}, {location.city}, {location.state} {location.zip_code}  - <a href={website}>{createReadableUrl(website)}</a> </Card.Title>
               <Col>
-                <Card.Subtitle>Yelp Rating: {rating} of 5 stars ({review_count} total reviews)</Card.Subtitle>
+                <Card.Subtitle><b>Yelp Rating:</b> {rating} of 5 stars ({review_count} total reviews)</Card.Subtitle>
                 <Card.Img variant="top" src={image_url} />
               </Col>
               <Col>
-                <Card.Subtitle>Google Rating: {googleRating} of 5 stars ({user_ratings_total} total reviews)</Card.Subtitle>
+                <Card.Subtitle><b>Google Rating:</b> {googleRating} of 5 stars ({user_ratings_total} total reviews)</Card.Subtitle>
                 <Card.Img variant="top" src={`${process.env.REACT_APP_PROXY_DOMAIN}google/maps/api/place/photo?maxwidth=400&photo_reference=${props.googleBiz.photos[0].photo_reference}&key=${process.env.REACT_APP_GOOGLE_API}`} />
               </Col>
             </Row>
